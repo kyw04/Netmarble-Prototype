de1a18353bdcc22c2c694dfcbdd994899c9e25bd
kyw04 <85551017+kyw04@users.noreply.github.com>
2022-09-22 00:51:33 +0900

    Revert "Revert "Merge branch 'kyw'""
    This reverts commit 1a01d03e3443c65346cb2c8516498d08f298e5d6.

---
+++ Assets/Dypsloom/RhythmTimeline/Scripts/SceneManagement/SceneLoader.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    [SerializeField] protected int m_SceneBuildIndex;
+
+    [ContextMenu("LoadScene")]
+    public void LoadScene()
+    {
+        LoadScene(m_SceneBuildIndex);
+    }
+
+    private void LoadScene(int buildIndex)
+    {
+        SceneManager.LoadScene(buildIndex);
+    }
+}
