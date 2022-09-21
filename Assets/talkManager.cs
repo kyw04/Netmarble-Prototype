324699eac912c185c6afdebad5c532b0c1859fe4
우다원 <105121244+woodawon@users.noreply.github.com>
2022-09-22 01:50:26 +0900

    대화 수정 - 1

--- Assets/talkManager.cs
+++ Assets/talkManager.cs
@@ -7,19 +7,25 @@ using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 using Debug = UnityEngine.Debug;
 using TMPro;
+using static System.Net.Mime.MediaTypeNames;
+/*using System;
+using static System.Net.Mime.MediaTypeNames;*/
 
 public class talkManager : MonoBehaviour
 {
     public GameObject talkPanel;
+    public GameObject nameTag;
     public TextMeshProUGUI text, textname;
     int clickCount = 0;
     public string SceneToLoad;
     string a = "girl1", b = "girl2";
+    public GameObject girl1;
+    public GameObject girl2;
+
 
 
     void Update()
     {
-        
         if (Input.GetMouseButtonDown(0))
         {
             if (clickCount == 0)
@@ -27,11 +33,14 @@ public class talkManager : MonoBehaviour
                 textname.text = a;
                 text.text = "hi.";
                 clickCount++;
+
+            } 
+            else if (clickCount == 1){
                 textname.text = b;
                 text.text = "Good.";
-                
+                clickCount++;
             }
-            else if (clickCount == 1)
+            else if (clickCount == 2)
             {
                 SceneManager.LoadScene(SceneToLoad);
             }
