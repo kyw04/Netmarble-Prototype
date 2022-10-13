49eedebacda37ed4b3760873255ce74d1eaa9182
우다원 <105121244+woodawon@users.noreply.github.com>
2022-10-13 22:51:57 +0900

    애니메이션(미완)
    이번주 내로 완성 올릴거임.

---
+++ Assets/Player_move.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Runtime;
+/*using System.Runtime.Remoting.Services;*/
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Player_move : MonoBehaviour
+{
+
+    SpriteRenderer spriteRenderer;
+    Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.touchCount >= 1)
+        {
+            Vector2 touchDeltaPosition = Input.GetTouch(0).position - Input.GetTouch(0).rawPosition;
+
+            if (touchDeltaPosition.x > 0.11)
+            {
+                if (touchDeltaPosition.y > -7.33)
+                {
+                    anim.SetBool("isOne", true);
+                    spriteRenderer.flipX = true;
+                }
+                else
+                {
+                    anim.SetBool("isOne", false);
+                }
+            }
+        }
+    }
+}
