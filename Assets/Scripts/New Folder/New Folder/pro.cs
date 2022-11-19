f36bc0f6e492d7a151fa5315a5afd9edcf4c8a17
우다원 <105121244+woodawon@users.noreply.github.com>
2022-11-19 19:59:09 +0900

    .

--- Assets/Scripts/New Folder/New Folder/pro.cs
+++ Assets/Scripts/New Folder/New Folder/pro.cs
@@ -1,12 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using static System.Net.Mime.MediaTypeNames;
 
 public class pro : MonoBehaviour
 {
     public GameObject[] images;
+    public Text tx;
     public Text m_Text;
     public string[] contents;
     public int index = 0;
@@ -15,6 +18,7 @@ public class pro : MonoBehaviour
     public void Start()
     {
         onclickbutton();
+        StartCoroutine(typing());
     }
     void Update()
     {
@@ -47,5 +51,14 @@ public class pro : MonoBehaviour
         images[index - 1].SetActive(false);
         images[index].SetActive(true);
         m_Text.text = contents[index];
+    }
+
+    IEnumerator typing()
+    {
+        for (int i = 0; i <= m_Text.Length; i++)
+        {
+            tx.text = m_Text.Substring(0, i);
+            yield return new WaitForSeconds(0.15f);
+        }
     }
 }
