0768fa41da15eb88c660940049b9681894230eae
prender546 <110472395+prender546@users.noreply.github.com>
2022-11-19 22:16:20 +0900

    .

---
+++ Assets/Scripts/New Folder/close.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class close : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    public void inm()
+    {
+        //CurtainAni.instance.Close();
+    }
+    public void outm()
+    {
+        //CurtainAni.instance.Open();
+    }
+}
